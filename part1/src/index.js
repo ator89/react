9847bcd544775169e673cf3b77e538b8c374f50e
Angel Torres <ator89@outlook.com>
2021-09-09 15:01:43 -0600

    Components

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello from Hello</p>
+    </div>
+  )
+}
 
-const App = () => (
-  <div>
-    <p>Hello World</p>
-  </div>
-)
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+
+  return(
+    <div>
+      <Hello />
+      <p>Hello World, it is {now.toString}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
